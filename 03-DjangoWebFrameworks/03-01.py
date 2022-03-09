6cff543eaf97688cadcf4e9870ddc5de5a2cf492
Toygoon <toygoon@gmail.com>
2022-03-09 20:25:28 +0900

    Ex03-01, Examples of URL.conf

---
+++ 03-DjangoWebFrameworks/03-01.py
@@ -0,0 +1,13 @@
+from importlib.resources import path
+""" Examples of URL.conf """
+
+from django.urls import path
+
+from . import views 
+
+urlpatterns = [
+    path('articles/2003/', views.special_case_2003),
+    path('articles/<int:year>/', views.year_archive),
+    path('articles/<int:year>/<int:month>/', views.month_archive),
+    path('articles/<int:year>/<int:month>/<slug:slug>/', views.article_detail),
+]
