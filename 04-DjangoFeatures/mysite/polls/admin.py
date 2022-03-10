ef8a5451b35318b3d684a64e70ebd3e3f16b5abe
Toygoon <toygoon@gmail.com>
2022-03-10 19:52:31 +0900

    Ex04-10

--- 04-DjangoFeatures/mysite/polls/admin.py
+++ 04-DjangoFeatures/mysite/polls/admin.py
@@ -26,12 +26,12 @@ class QuestionAdmin(admin.ModelAdmin):
             'classes' : ['collapse'],
         })
     ]
+    # Add filter sidebar
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
-# Add filter sidebar
-inlines = [ChoiceInline]
-list_display = ('question_text', 'pub_date')
-list_filter = ['pub_date']
