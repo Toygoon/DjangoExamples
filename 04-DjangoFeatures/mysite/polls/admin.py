b9a6ede4c1a560e24cc79b6079ba4d7271e49355
Toygoon <toygoon@gmail.com>
2022-03-10 19:13:38 +0900

    Ex04-03, Show separated fields

--- 04-DjangoFeatures/mysite/polls/admin.py
+++ 04-DjangoFeatures/mysite/polls/admin.py
@@ -5,6 +5,11 @@ from polls.models import Question, Choice
 class QuestionAdmin(admin.ModelAdmin):
     # Change field sequences
     fields = ['pub_date', 'question_text']
+    # Show separated fields
+    fieldsets = [
+        ('Question Statement', {'fields' : ['quesiton_text']}),
+        ('Date Information', {'fields' : ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
