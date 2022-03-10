e04218e9e0a2c1b8211456cff5f29f19a5c342a6
Toygoon <toygoon@gmail.com>
2022-03-10 19:26:51 +0900

    Ex04-05, Show Question and Choice together

--- 04-DjangoFeatures/mysite/polls/admin.py
+++ 04-DjangoFeatures/mysite/polls/admin.py
@@ -8,8 +8,25 @@ class QuestionAdmin(admin.ModelAdmin):
     # Show separated fields
     fieldsets = [
         ('Question Statement', {'fields' : ['quesiton_text']}),
-        ('Date Information', {'fields' : ['pub_date']}),
+        ('Date Information', {
+            'fields' : ['pub_date'],
+            'classes' : ['collapse']}),
     ]
+    
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    # Set how many empty fields are showed
+    extra = 2
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields' : ['question_text']}),
+        ('Date information', {
+            'fields' : ['pub_date'],
+            'classes' : ['collapse'],
+        })
+    ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
