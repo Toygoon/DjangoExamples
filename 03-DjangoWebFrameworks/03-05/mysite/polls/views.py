2242515f6cd478d556e6543818163434ab8ec8f5
Toygoon <toygoon@gmail.com>
2022-03-10 18:41:41 +0900

    typo

--- 03-DjangoWebFrameworks/03-05/mysite/polls/views.py
+++ 03-DjangoWebFrameworks/03-05/mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
-from polls.models import Question
+from polls.models import Question, Choice
 
 # Create your views here.
 
