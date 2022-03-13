57e2b79802b09c516145b07f731d9ea88946671d
Toygoon <toygoon@gmail.com>
2022-03-13 15:01:48 +0900

    typo

--- 05-ExtendedExamples/mysite/books/views.py
+++ 05-ExtendedExamples/mysite/books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
 from books.models import Book, Author, Publisher
 
