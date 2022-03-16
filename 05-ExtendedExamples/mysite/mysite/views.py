53a6942e77aa7dfc239e835bbfd065e08463d322
Toygoon <toygoon@gmail.com>
2022-03-16 18:05:42 +0900

    explanations

--- 05-ExtendedExamples/mysite/mysite/views.py
+++ 05-ExtendedExamples/mysite/mysite/views.py
@@ -9,9 +9,13 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         # context['app_list'] = ['polls', 'books']
         dictVerbose = {}
+        
+        # get_app_configs() loads the list registered in INSTALLED_APPS from settings.py
         for app in apps.get_app_configs():
+            # 'site-packages' is the part of settings, so we need to exclude this in relative directories
             if 'site-packages' not in app.path:
                 dictVerbose[app.label] = app.verbose_name
+        # When doen with for loop above, put the dictVerbose to the context in verbose_dict
         context['verbose_dict'] = dictVerbose
         
         return context
