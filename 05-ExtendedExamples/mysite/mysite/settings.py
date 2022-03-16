f0708702441b22764f5ecdc491fb7adae4df410a
Toygoon <toygoon@gmail.com>
2022-03-16 18:08:45 +0900

    Ex05-23, edit setings.py

--- 05-ExtendedExamples/mysite/mysite/settings.py
+++ 05-ExtendedExamples/mysite/mysite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'books.apps.BookConfig',
+    #'books.apps.BookConfig',
+    'books',
 ]
 
 MIDDLEWARE = [
