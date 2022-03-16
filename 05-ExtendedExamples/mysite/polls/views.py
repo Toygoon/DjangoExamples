058a655e601883db694383d7635f18afc3a565b2
Toygoon <toygoon@gmail.com>
2022-03-16 19:52:29 +0900

    Ex05-25, Change polls application

--- 05-ExtendedExamples/mysite/polls/views.py
+++ 05-ExtendedExamples/mysite/polls/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.views import generic
 from polls.models import Question, Choice
 
 # Create your views here.
-
-def index(request):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    
-    return render(request, 'polls/detail.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -39,3 +29,19 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     
     return render(request, 'polls/results.html', {'question': question})
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'last_question_list'
+    
+    def get_queryset(self):
+        # Return the 5 questions generated recently
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+class ResultView(generic.ResultView):
+    model = Question
+    template_name = 'polls/result.html'
